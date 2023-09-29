8de17fe591cab551d8da529809bb766674ae2244
mouhcineezzaidi <126358536+mouhcineezzaidi@users.noreply.github.com>
2023-09-30 00:02:47 +0100

    Python - Network #1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,7 +7,7 @@ from sys import argv
 
 if __name__ == "__main__":
     request = requests.get(argv[1])
-    if request.status_code >= 400:
+    if request.status_code >= 500:
         print("Error code: {}".format(request.status_code))
     else:
         print(request.text)
